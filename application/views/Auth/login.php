e524c24f76040068ab390ff1ee7fae81991c680f
DaveCybr <davidrngg4@gmail.com>
2023-09-04 13:53:08 +0700

    config

--- application/views/Auth/login.php
+++ application/views/Auth/login.php
@@ -10,7 +10,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
   <link href="<?= base_url(); ?>dist/img/logo/logo.png" rel="icon">
-  <title>RuangAdmin - Login</title>
+  <title>eklinik | Login</title>
   <link href="<?= base_url(); ?>dist/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="<?= base_url(); ?>dist/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
   <link href="<?= base_url(); ?>dist/css/ruang-admin.min.css" rel="stylesheet">
