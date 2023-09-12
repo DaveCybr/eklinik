b9c0e5a934c7cbed7885d25974dff096e6a8f32c
DaveCybr <davidrngg4@gmail.com>
2023-09-12 08:28:10 +0700

    dist

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -21,15 +21,15 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
     <!-- Libraries Stylesheet -->
-    <link href="<?php echo base_url(); ?>lib/animate/animate.min.css" rel="stylesheet">
-    <link href="<?php echo base_url(); ?>lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
-    <link href="<?php echo base_url(); ?>lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+    <link href="<?php echo base_url(); ?>dist/landingpage/lib/animate/animate.min.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>dist/landingpage/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>dist/landingpage/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
 
     <!-- Customized Bootstrap Stylesheet -->
-    <link href="<?php echo base_url(); ?>css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>dist/landingpage/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Template Stylesheet -->
-    <link href="<?php echo base_url(); ?>css/style.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>dist/landingpage/css/style.css" rel="stylesheet">
 </head>
 
 <?php echo $this->load->view($body); ?>
@@ -102,17 +102,17 @@
     <!-- JavaScript Libraries -->
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="<?php echo base_url(); ?>lib/wow/wow.min.js"></script>
-    <script src="<?php echo base_url(); ?>lib/easing/easing.min.js"></script>
-    <script src="<?php echo base_url(); ?>lib/waypoints/waypoints.min.js"></script>
-    <script src="<?php echo base_url(); ?>lib/counterup/counterup.min.js"></script>
-    <script src="<?php echo base_url(); ?>lib/owlcarousel/owl.carousel.min.js"></script>
-    <script src="<?php echo base_url(); ?>lib/tempusdominus/js/moment.min.js"></script>
-    <script src="<?php echo base_url(); ?>lib/tempusdominus/js/moment-timezone.min.js"></script>
-    <script src="<?php echo base_url(); ?>lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+    <script src="<?php echo base_url(); ?>dist/landingpage/lib/wow/wow.min.js"></script>
+    <script src="<?php echo base_url(); ?>dist/landingpage/lib/easing/easing.min.js"></script>
+    <script src="<?php echo base_url(); ?>dist/landingpage/lib/waypoints/waypoints.min.js"></script>
+    <script src="<?php echo base_url(); ?>dist/landingpage/lib/counterup/counterup.min.js"></script>
+    <script src="<?php echo base_url(); ?>dist/landingpage/lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="<?php echo base_url(); ?>dist/landingpage/lib/tempusdominus/js/moment.min.js"></script>
+    <script src="<?php echo base_url(); ?>dist/landingpage/lib/tempusdominus/js/moment-timezone.min.js"></script>
+    <script src="<?php echo base_url(); ?>dist/landingpage/lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
 
     <!-- Template Javascript -->
-    <script src="<?php echo base_url(); ?>js/main.js"></script>
+    <script src="<?php echo base_url(); ?>dist/landingpage/js/main.js"></script>
 </body>
 
 </html>
